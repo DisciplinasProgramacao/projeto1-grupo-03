e525023c882b516c7e07bd2ba2a09871e3efa912
Vitor Lany <vitorlany99@gmail.com>
2023-02-26 09:26:48 -0300

    Project Architecture

---
+++ codigo/gestao_de_estoque/src/test/java/org/example/AppTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void exampleTest() {
+        System.out.println("Working");
+        Assert.fail();
+    }
+}
