9571783cce83fd577e04aa0b183f9b79e13370a7
Vitor Lany <vitorlany99@gmail.com>
2023-02-27 10:30:28 -0300

    Exemplo de caso de teste

--- codigo/gestao_de_estoque/src/main/java/com/project/App.java
+++ codigo/gestao_de_estoque/src/main/java/com/project/App.java
@@ -1,11 +1,7 @@
 package com.project;
 
-import com.project.domain.Produto;
-
 public class App {
     public static void main(String[] args) {
-        Produto produto = new Produto();
-        produto.setLucroPorcentagem(0.9);
         System.out.println("Hello World!");
     }
 }
--- codigo/gestao_de_estoque/src/test/java/com/project/AppTest.java
+++ codigo/gestao_de_estoque/src/test/java/com/project/AppTest.java
@@ -1,13 +1,34 @@
 package com.project;
 
-import org.junit.Assert;
+import com.project.domain.Produto;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AppTest {
 
-//    @Test
-    public void exampleTest() {
-        System.out.println("Working");
-        Assert.fail();
+    private Produto produto;
+
+    /**
+     * Configura a instância de produto para cada novo teste a ser rodado
+     */
+    @Before
+    public void setUp() {
+        produto = new Produto();
+    }
+
+    /**
+     * Verifica as bordas se não está menor os 30% pedidos como requisito
+     */
+    @Test(expected = RuntimeException.class)
+    public void lucroPorcentagemMenorTest() {
+        produto.setLucroPorcentagem(0.29);
+    }
+
+    /**
+     * Verifica as bordas se não está estrapolando os 80% pedidos como requisito
+     */
+    @Test(expected = RuntimeException.class)
+    public void lucroPorcentagemMaiorTest() {
+        produto.setLucroPorcentagem(0.81);
     }
 }
