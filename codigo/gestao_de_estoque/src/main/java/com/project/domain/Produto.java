ce4eacf4fd33cfad5dcf8e25b64c09d1cc288899
Vitor Lany <vitorlany99@gmail.com>
2023-02-27 10:19:51 -0300

    Exemplo de documentação

--- codigo/gestao_de_estoque/src/main/java/com/project/domain/Produto.java
+++ codigo/gestao_de_estoque/src/main/java/com/project/domain/Produto.java
@@ -12,4 +12,17 @@ public class Produto {
     double precoVenda;
     double lucroPorcentagem;
     int estoque;
+
+    /**
+     * Lucro deve ser entre 30% e 80%
+     * @param lucroPorcentagem exemplo 0.3
+     * @exception RuntimeException
+     */
+    public void setLucroPorcentagem(double lucroPorcentagem) {
+        if (lucroPorcentagem >= 0.3 && lucroPorcentagem <= 0.8) {
+            this.lucroPorcentagem = lucroPorcentagem;
+        } else {
+            throw new RuntimeException("Porcentagem inválida.");
+        }
+    }
 }
