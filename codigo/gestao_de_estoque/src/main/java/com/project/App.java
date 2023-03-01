2b27ec0211db03c9f453ddcae87396315f28d949
Baldiotti <augustobaldiotti@hotmail.com>
2023-03-01 19:18:22 -0300

    Commit teste Eclipse

--- codigo/gestao_de_estoque/src/main/java/com/project/App.java
+++ codigo/gestao_de_estoque/src/main/java/com/project/App.java
@@ -2,6 +2,6 @@ package com.project;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("Hello World!");
     }
 }
