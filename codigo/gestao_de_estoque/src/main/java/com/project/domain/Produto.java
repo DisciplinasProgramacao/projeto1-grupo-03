868d95a31c133d0be081b9b5d383125502415d4a
Vitor Lany <vitorlany99@gmail.com>
2023-03-07 17:33:53 -0300

    Refatoração #17

--- codigo/gestao_de_estoque/src/main/java/com/project/domain/Produto.java
+++ codigo/gestao_de_estoque/src/main/java/com/project/domain/Produto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Produto {
-    static double IMPOSTOS_PORCENTAGEM = 0.18;
+    static final double IMPOSTOS_PORCENTAGEM = 0.18;
 
     String descricao;
     double precoCusto;
@@ -31,7 +31,6 @@ public class Produto {
     /**
      * Lucro deve ser entre 30% e 80%
      * @param lucroPorcentagem exemplo 0.3
-     * @exception RuntimeException
      */
     public void setLucroPorcentagem(double lucroPorcentagem) {
         if (lucroPorcentagem >= 0.3 && lucroPorcentagem <= 0.8) {
